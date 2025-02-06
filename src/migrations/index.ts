4d3e8fd57aa387e154e2da46056346af03480295
Andrii Semenystyi <46621668+MeowningMaster@users.noreply.github.com>
2025-02-07 01:32:37 +0200

    chore: migrations

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20250206_233104 from './20250206_233104';
+
+export const migrations = [
+  {
+    up: migration_20250206_233104.up,
+    down: migration_20250206_233104.down,
+    name: '20250206_233104'
+  },
+];
