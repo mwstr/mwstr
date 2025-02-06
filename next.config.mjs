c0bd3bb00e1a72a778d076f8f13f5129b5818ab0
Andrii Semenystyi <46621668+MeowningMaster@users.noreply.github.com>
2025-02-07 00:34:40 +0200

    chore: init3

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,8 @@ import { withPayload } from '@payloadcms/next/withPayload'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    // ppr: "incremental",
+    ppr: 'incremental',
+    turbo: true,
   },
 }
 
